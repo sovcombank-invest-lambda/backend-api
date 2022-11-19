02539a1237239627cd190ce5f523fdbc54b1d295
huscker <egostalker03@mail.ru>
2022-11-19 09:50:49 +0300

    Added simple conversion from orm to pydantic

--- service/schemas/currency_account.py
+++ service/schemas/currency_account.py
@@ -15,6 +15,8 @@ class CurrencyAccountOut(BaseModel):
     name: str = Field(..., description='Название валютного счета')
     value: float = Field(..., description='Состояние счета')
     created_at: datetime = Field(..., description='Дата создания')
+    class Config:
+            orm_mode = True
     
 class CurrencyAccountDelete(BaseModel):
     currency_account_id: UUID = Field(..., description="UUID валютного счета")
@@ -24,6 +26,8 @@ class Currency(BaseModel):
     name: str = Field(..., description='сокр. название валюты')
     fullname: str = Field(..., description='Полное название валюты')
     value: float = Field(..., description='Курс валюты')
+    class Config:
+            orm_mode = True
     
 class CurrencyTransaction(BaseModel):
     change_value: float = Field(..., description="Изменение средств на счете")
